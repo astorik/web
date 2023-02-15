5419fdbbb70ad88a0acfe9d5e835635bf95fa8f8
turtleDev <saravjeetamansingh@gmail.com>
2023-02-15 23:36:53 +0530

    some brand adjustments

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import './App.css';
-import icon from './astorik.jpg'
 
 function App() {
   return (
     <div className="container font-sans">
-      <div className="flex justify-between items-center w-screen bg-indigo-400 h-24 px-7">
-        <img src={icon} alt="Astorik" className="h-1/2 w-auto"/>
-        <div className="container">
-          <p className="text-xl font-bold text-gray-100">user@host.com</p>
-        </div>
+      <div className="flex justify-between items-center w-screen bg-purple-100 h-24 px-8 py-10">
+        <p className="text-black text-4xl font-black">astorik</p>
       </div>
     </div>
   );
