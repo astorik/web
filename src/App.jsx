e69708e71450b5e475afe388814a47b444fedbcf
turtleDev <saravjeetamansingh@gmail.com>
2023-03-08 17:00:35 +0530

    wip on loading user data

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import './App.css';
+import React from 'react';
 
-function App() {
+function Nav() {
   return (
-    <div className="container font-sans">
+    <div className="container">
       <div className="flex justify-between items-center w-screen bg-purple-100 h-24 px-8 py-10">
-        <p className="text-black text-4xl font-black">astorik</p>
+        <p className="text-4xl font-semibold tracking-tight scale-y-90">astorik</p>
       </div>
     </div>
   );
+}
+
+async function App() {
+  // todo: figure how to make async requests work
+  let userData = await fetch("http://localhost:8000/v0/user/hello@turtledev.in");
+  return (
+    <React.Fragment>
+      <Nav></Nav>
+      <div className="container">
+        <p>text</p>
+      </div>
+    </React.Fragment>
+  )
 }
 
 export default App;
