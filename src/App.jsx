d6727a62fb12b7170fc6e04924f3f1a65e024c13
turtleDev <saravjeetamansingh@gmail.com>
2023-03-13 17:23:31 +0530

    app: integrate router; add homescreen

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import './App.css';
 import React, {useEffect, useState} from 'react';
+import { createBrowserRouter, useRouteError, Outlet, Link } from 'react-router-dom';
 
 function Nav({email}) {
   return (
-    <div className="container">
+    <div className="">
       <div className="flex justify-between items-center w-screen bg-purple-100 h-24 px-8 py-10">
-        <p className="text-4xl font-semibold tracking-tight scale-y-90">astorik</p>
+        <Link to="/" className="text-4xl font-semibold tracking-tight scale-y-90">astorik</Link>
         <p>{email}</p>
+      </div>
+      <div className="main">
+        <Outlet/>
       </div>
     </div>
   );
 }
 
-// todo: refactor
 function Preferences({user}) {
   let orgs = user.Orgs ?? [];
   let preferenceCentres = orgs.map(org => {
@@ -61,10 +64,63 @@ function App() {
   
   return (
     <div>
-        <Nav email={userData.Email}></Nav>
         <Preferences user={userData}/>
     </div>
   );
 }
 
-export default App;
+function ErrorPage() {
+  const error = useRouteError();
+  console.log(error);
+  return (
+    <div className="container h-screen w-screen flex justify-around items-center flex-col">
+      <div className="-translate-x-2/4 -translate-y-2/4">
+        <p className="text-2xl font-bold">Error</p>
+        <p>{error.statusText || error.message}</p>
+        <p>Go <Link to="/" className="text-red-400">home</Link></p>
+      </div>
+    </div>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <div className="p-6 flex flex-col items-center">
+        <div className="font-mono text-xl whitespace-pre">{hammerTime}</div>
+        <h1 className="text-2xl font-light">Work In Progress</h1>
+        <Link to="/preferences" className="btn-primary mt-20">Click here for demo</Link>
+      </div>
+    </div>
+  )
+}
+
+const hammerTime =`
+T                                    \\\`.    T
+|    T     .--------------.___________) \   |    T
+!    |     |//////////////|___________[ ]   !  T |
+     !     \`--------------'           ) (      | !
+                                  mn  '-'      !
+`
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Nav/>,
+    errorElement: <ErrorPage/>,
+    children: [,
+      {
+        path: "/",
+        element: <Home/>
+      },
+      {
+        path: "/preferences",
+        element: <App/>
+      }
+    ],
+  }
+], {
+  basename: "/web"
+});
+
+export default router;
